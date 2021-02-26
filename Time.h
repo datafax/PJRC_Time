31f3f21323c5080ffc1a2bead65b860de7949964
datafax <electronicsdeveloper@protonmail.com>
2021-02-26 16:58:16 -0700

    removed warning from time.h

--- Time.h
+++ Time.h
@@ -1,2 +1,6 @@
-#warning "Please include TimeLib.h, not Time.h.  Future versions will remove Time.h"
+/* This file ensures backwards compatibility with other libraries that */
+/* attempt to include "time.h". */
+/* To prevent unpredictable results, */
+/* the following folder should be removed: */
+/* C:\arduino-1.8.13\hardware\teensy\avr\libraries\Time */
 #include "TimeLib.h"
